d822b33d0227cc55c0f4943920fcc84a40ac26c9
sfcdota <sfcdota@gmail.com>
2021-03-20 01:14:08 +0300

    cpp to 07 done

--- cpp_module_07/ex02/Array.hpp
+++ cpp_module_07/ex02/Array.hpp
@@ -5,15 +5,52 @@
 template <typename T>
 class Array{
 	public:
-		Array (void);
-		Array(unsigned int n);
-		Array(Array const & a);
-		Array & operator=(Array const & a);
-		T & operator[](unsigned index);
-		unsigned getSize(void) const;
-		~Array();
+		Array (void)
+		{
+			arr = nullptr;
+			size = 0;
+		}
+		Array(unsigned int n)
+		{
+			arr = new T[n]();
+			size = n;
+		}
+		Array(Array const & a)
+		{
+			*this = a;
+		}
+		Array & operator=(Array const & a)
+		{
+			if (a == this)
+				return *this;
+			if (size != a.getSize())
+				{
+					delete [] arr;
+					size = a.getSize();
+					arr = new T[size]();
+				}
+			for (unsigned i = 0; i < size; i++)
+				arr[i] = a[i];
+		}
+		T & operator[](unsigned index)
+		{
+			if (index < 0 || index >= size)
+				throw std::exception();
+			else
+				return arr[index];
+		}
+		unsigned getSize(void) const
+		{
+			return size;
+		}
+		~Array()
+		{
+			if (arr)
+				delete [] arr;
+		}
 	private:
 		T* arr;
 		unsigned size;
 };
+
 #endif
